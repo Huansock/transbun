b484f618649b221af45eff4a9ecd61610e048bbb
Schlingel <Huan.sock@gmail.com>
2023-03-18 18:26:41 +0100

    fertig code

---
+++ index.ts
@@ -0,0 +1,45 @@
+import { serve, file } from "bun";
+interface Data {
+    translatedText: string;
+  }
+
+const server = Bun.serve({
+    port:3000,
+    fetch(req , server) {
+        // upgrade the request to a WebSocket
+        if (server.upgrade(req)) {
+            return; // do not return a Response
+        }
+    const url = new URL(req.url);
+    if (url.pathname === "/") return new Response(Bun.file("./index.html"));
+    if (url.pathname === "/script.js") return new Response(Bun.file("./script.js"));
+    return new Response(`404!`);
+    },
+    websocket: {
+        async message(ws, message) {
+            const jsonString = Buffer.from(message).toString('utf8')
+            const parsedData = JSON.parse(jsonString)
+            const resourceData = JSON.stringify(parsedData.data)
+            const res = await fetch("https://transwooseok.fun/translate", {
+                method: "POST",
+                body: JSON.stringify({
+                  q: resourceData,
+                  source: "de",
+                  target: "en"
+                }),
+                headers: { "Content-Type": "application/json" }
+              });
+                 const en_json = await res.json() as Data;
+                 const en_text = JSON.stringify(en_json["translatedText"])   
+                 ws.send(en_text.slice(3,-3)); // echo back the message
+        }, // a message is received
+        open(ws) {}, // a socket is opened
+        close(ws, code, message) {}, // a socket is closed
+        drain(ws) {}, // the socket is ready to receive more data
+    }, // handlers
+  });
+
+  
+  
+  console.log(`Listening on http://localhost:${server.port}...`);
+  
---
+++ script.js
@@ -0,0 +1,122 @@
+const german = document.querySelector('#german');
+const english = document.querySelector('#korean');
+const checking = document.querySelector('#checking');
+const btn_end = document.querySelector('#btn_end');
+const btn_start = document.querySelector('#btn_start');
+const script = document.querySelector('#script');
+const myList = document.getElementById("myList");
+const socket = new WebSocket("ws://localhost:3000");
+let mysrcipt = [];
+
+//socket setting
+// message is received
+socket.addEventListener("message", event => {
+  console.log(event.data)
+  translatedMessage = JSON.stringify(event.data);
+  mysrcipt.push(translatedMessage);
+  english.innerHTML = translatedMessage;
+
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(updateTime()+":     "+translatedMessage));
+  myList.appendChild(li);
+});
+
+// socket opened
+socket.addEventListener("open", event => {
+  console.log("Connected to server");
+  socket.send(JSON.stringify({ type: "message", data: "Hello Server!" }));
+});
+
+// socket closed
+socket.addEventListener("close", event => {});
+
+// error handler
+socket.addEventListener("error", event => {});
+
+
+// 현재시간
+let currentTimeElement = document.getElementById('currentTime');
+function updateTime() {
+  let currentTime = new Date().toLocaleTimeString();
+  currentTimeElement.textContent = currentTime;
+  return currentTime;
+}
+setInterval(updateTime, 1000);
+
+
+// 음성인식 객체 생성
+
+let SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+function 한국어집어넣기(한국어번역) {
+    document.getElementById("translated").innerHTML = 한국어번역;
+  }
+  
+  // 메세지를 주는 함수
+  const message = async (fin_text, interim, isfinal) => {
+    let de_text = `${fin_text || interim}`;
+    german.innerHTML = de_text;
+    if (isfinal) socket.send(JSON.stringify({ type: "message", data: de_text }));
+  }
+
+  
+  // 브라우저 체킹 함수
+  function checkCompatibility() {
+    if (!recognition) {
+      checking.innerHTML =
+        "recgnotion is not supported by your browser.";
+    } else {
+      checking.innerHTML =
+        "recognition is supported by your browser.";
+    }
+  }
+  //  끝나는 이벤트에 대한 핸들러
+
+    function onend(e) {
+      recognition.start();
+    }
+
+  //  결과값 이벤트 핸들러
+  function onresult(event) {
+    let fin_text = "";
+    let interim = "";
+    let isfinal = false;
+    // console.log(event.results.length)
+    // console.log(event.results)
+    // console.log(event.results.transcript)
+  
+    for (let i = 0; i < event.results.length; i++) {
+      let res = event.results[i];
+      let trans = res[0].transcript;
+      if (res.isFinal) {
+        fin_text += trans;
+        isfinal = true;
+      } else {
+        interim += trans;
+      }
+    }
+    message(fin_text.trim(), interim.trim(), isfinal);
+  }
+  
+  function onstart(e) {
+    checkCompatibility();
+  }
+  btn_start.onclick = () => {
+    recognition.start();
+
+  };
+  btn_end.onclick = () =>{
+
+    recognition.stop();
+  };
+  // 기본적인 음성인식 설정
+  recognition.lang = "de";
+  recognition.continuous = false;
+  recognition.interimResults = true;
+  recognition.maxAlternatives = 10000;
+  recognition.onresult = onresult;
+  recognition.onend = onend;
+  recognition.onstart = onstart;
+  recognition.start();
